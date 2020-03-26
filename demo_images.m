dd00a87708059f9dd156331ec2ca11bb4fb4f581
Mahmoud <37669469+mahmoudnafifi@users.noreply.github.com>
2020-03-26 02:41:01 -0400

    create a demo for batch processing

---
+++ demo_images.m
@@ -0,0 +1,39 @@
+%% input image dir info.
+input_dir = 'input_images';
+input_image_ext = '.jpg';
+
+%% output image dir info.
+output_dir = 'results';
+
+if exist(output_dir,'dir') == 0
+    mkdir(output_dir);
+end
+
+images = dir(fullfile(input_dir,['*' input_image_ext]));
+
+images = {images(:).name};
+
+%% loading our model
+load(fullfile('models','model.mat'));
+
+for i = 1 : length(images)
+    fprintf('processing %s ... \n',images{i});
+    imageName = fullfile(input_dir,images{i});
+    
+    %% reading image
+    I = im2double(imread(imageName));
+    sz = size(I);
+    
+    %% gray-world estimation and correction
+    ill = illumgray(I); ill = ill./norm(ill); %estimate illuminant using GW
+    D = ill(2)./ill; %diagonal correction matrix
+    
+    %% our post-processing
+    d = pdist2(ill,model.C,'cosine');
+    [~,cids] = sort(d); cid = cids(1);
+    M = reshape(D * reshape(model.B(cid,:),[3,33]),[11,3]);
+    I_corr = out_of_gamut_clipping(...
+        reshape(PHI(reshape(I,[],3)) * M,[sz(1),sz(2),sz(3)]));
+    
+    imwrite(I_corr,fullfile(output_dir,images{i}));
+end
